0b5b1bd1a42137c507642d36e81b1ab4e85cf513
luong228 <90311663+luong228@users.noreply.github.com>
2023-02-22 13:28:41 +0700

    Implement Identity for security
    Changes:
    - Get UserName, Role from JWT Token
    - Add role if does not exists
    - Update all packages up to 7.03
    - Remove property Role out of LoginResponseDTO

---
+++ MagicVilla_VillaAPI/Migrations/20230221163444_AddIdentityUserToDb1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVillaVillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIdentityUserToDb1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 2, 21, 23, 34, 44, 169, DateTimeKind.Local).AddTicks(4773));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 2, 21, 23, 34, 44, 169, DateTimeKind.Local).AddTicks(4792));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2023, 2, 21, 23, 34, 44, 169, DateTimeKind.Local).AddTicks(4794));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2023, 2, 21, 23, 34, 44, 169, DateTimeKind.Local).AddTicks(4796));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2023, 2, 21, 23, 34, 44, 169, DateTimeKind.Local).AddTicks(4797));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 2, 21, 23, 19, 59, 376, DateTimeKind.Local).AddTicks(2121));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 2, 21, 23, 19, 59, 376, DateTimeKind.Local).AddTicks(2137));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2023, 2, 21, 23, 19, 59, 376, DateTimeKind.Local).AddTicks(2139));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2023, 2, 21, 23, 19, 59, 376, DateTimeKind.Local).AddTicks(2140));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2023, 2, 21, 23, 19, 59, 376, DateTimeKind.Local).AddTicks(2141));
+        }
+    }
+}
--- MagicVilla_VillaAPI/Repository/IRepository/IUserRepository.cs
+++ MagicVilla_VillaAPI/Repository/IRepository/IUserRepository.cs
@@ -5,8 +5,8 @@ namespace MagicVilla_VillaAPI.Repository.IRepository
 {
     public interface IUserRepository
     {
-        bool IsUniqueUser(string username);
+        Task<bool> IsUniqueUser(string username);
         Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
-        Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO);
+        Task<UserDTO> Register(RegisterationRequestDTO registerationRequestDTO);
     }
 }
